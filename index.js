7226230e651c1a2c2169e49921a648e8f3369cc8
renatocuellar <matealva@gmail.com>
2021-01-20 12:29:29 -0500

    Arrow functions y promesas

--- index.js
+++ index.js
@@ -103,3 +103,58 @@ const a = "Soy constante";
 a = "No soy constante" ; // Al ejecutar arroja error porque no puede cambiarse una constante luego de declararse.
 
 console.log(a);
+
+
+//Propiedad de objetos mejorada.
+
+let name =" Alejandro";
+let nick =" Alejandro-sin";
+
+ObjES6={name, nick}; //Nos permite ahorrar código para objetos muy grandes.
+console.log(ObjES6);
+
+//ArrowFunction
+
+const names= [
+    {name:"Alejandro",nick:"Alejandro-sin"},
+    {name:"etrx",nick:"xrte"}
+]
+
+//Antes se iteraba mediante map y se usaban funciones anónimas.
+
+let lista = names.map(function(item){
+    console.log(item.names);
+})
+
+//Ahora esta función anónima se reemplaza por => un arrow, flecha que apunta a.
+
+let listaES6 = names.map(item =>console.log(item.names));{ 
+}
+
+const listaES6_const = (name, nick) =>{
+    ...
+}
+
+const listaES6_const_único_elemento = name =>{
+    ...
+}
+
+const square = num => num *num; // Se evita el uso de return.
+
+// Promesas, sirven para trabajar el asincronismo y hacer peticiones a una API. 
+//Cómo su nombre indica dice que algo va a suceder.
+
+const helloPromise =() =>{
+    return new Promise ((resolve,reject)=>{
+        if(true){
+            resolve('Resuelto');
+        } else {
+            reject('Rechazado');
+        }
+    })
+}
+
+helloPromise()
+    .then(Response => console.log(Response))
+    .catch(error=> console.log(error));
+   
