00d26b7c590b4e4ee82e7b2eac1df720e360b729
renatocuellar <matealva@gmail.com>
2021-01-20 14:51:44 -0500

    Se añade todas las actualizaciones de ES8, ES9 y ES10

--- index.js
+++ index.js
@@ -265,3 +265,137 @@ const data= {
     front:'Alej', // Puede existir
     back: 'Rel'
 }
+
+
+
+// Async Await
+const prepareIceCream = (money) => {
+    return new Promise((resolve, reject) => {
+        if(money >= 500) {    
+            console.log('We are preparing your Ice cream...')
+            setTimeout(() => {
+                resolve('Here you are a Chocolate Ice cream');
+            }, 6000);  //3. Si el dinero que ingresaste es mayor o igual a 500, entonces te preparamos el helado. El proceso toma 6 segundos.
+        } else {
+            reject('Sorry! You do not have enough money :('); //3. Si el dinero es menor a 500, lo sentimos, no te alcanza
+        }
+    });
+}
+
+const buyIceCream = async (money) => { 
+    try{
+        console.log('Welcome to Ice Cream World!');
+        const result = await prepareIceCream(money); //2. Esperas aquí mientras envíamos el dinero, lo verifican y te preparan el helado 
+        console.log(result); //4. Imprimimos tu pedido! si todo salió bien.
+        console.log('Thanks for your purchase!');
+    } catch(e) {
+        console.log(e); //4. Te decimos que no salió bien el proceso porque tu dinero no es suficiente.
+    }
+}
+
+buyIceCream(600); //1. llamas a la funcion comprar helado e Ingresas el dinero
+
+
+//EMCAScript9 (ES9)
+
+/**
+ * Spread Operator
+ */
+const obj = {
+    name: 'Oscar',
+    age: 32,
+    country: 'MX'
+  };
+  
+  let { name, ...addInfo } = obj;
+  console.log(`name: ${name}`);
+  console.log(`additional information: `, addInfo);
+  
+  let { country, ...nameAndAge } = obj;
+  console.log(`name and age: `, nameAndAge);
+  
+  /**
+   * Porpagation Properties
+   */
+  const person = {
+    name: 'Oscar',
+    age: 32
+  };
+  
+  const personInformation = {
+    ...person,
+    country: 'MX'
+  };
+  console.log(`personInformation: `, personInformation);
+  
+  /**
+   * Promise Finally
+   */
+  const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+      true
+        ? setTimeout(() => {
+            resolve('Hello World!');
+          }, 3000)
+        : reject(new Error('Test Error'));
+    });
+  };
+  
+  helloWorld()
+    .then(result => console.log('result -> ', result))
+    .catch(err => console.log('err -> ', err))
+    .finally(() => console.log('finalizó'));
+  
+  /**
+   * Regex
+   */
+  const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+  const match = regexData.exec('2018-04-28');
+  const year = match[1];
+  const month = match[2];
+  const day = match[3];
+  console.log('Date -> ', year, month, day);
+
+//ECMASCRIPT10 (ES10)
+
+
+  //--------------method flat---------------/
+//devolver una matriz con una submatriz aplanada, 
+//recibe como argunmento la profundidad
+let array = [1,2,3, [1,2,3, [1,2,3]]];
+
+console.log(array.flat(2))
+
+//-------------flatMap---------------------------/
+//mapear cada elemento, luego pasarle una funcion y aplanar
+
+let array = [1,2,3,4,5];
+
+console.log(array.flatMap(value => [value, value * 2]));
+
+//----------------trim---------------------------/
+let hello = '                      hello world';
+console.log(hello);
+console.log(hello.trimStart()); // inicio
+
+let hello = 'hello world                  ';
+console.log(hello);
+console.log(hello.trimEnd()); // final
+
+//-------------optional catch biding-------------/
+
+try {
+    
+} catch /*(error) ya no es necesario colocarlo*/ {
+    error
+}
+
+//-------------fromEntries----------------------/
+// array to object
+let entries = [["name", "oscar"], ["age", 32]];
+console.log(Object.fromEntries(entries))
+
+//-------------symbol object--------------------/
+let mySymbol = 'My Symbol';
+let symbol = Symbol(mySymbol);
+console.log(symbol.description);```
